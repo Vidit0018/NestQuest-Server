4b2647ff467cf5b79c81e0499a514f5b3e68016b
Vidit Prajapati <vidit.msit@gmail.com>
2024-05-26 19:00:20 +0530

    utility added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
-import { errorHandeler } from "./error.js";
+import { errorHandeler } from "../utils/error.js";
 export const signup = async (req,res,next)=>{
     const {username,email,password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);
