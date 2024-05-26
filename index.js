724c96b67a3b42f8c44fd35de7891b76381802b7
Vidit Prajapati <vidit.msit@gmail.com>
2024-05-26 17:17:18 +0530

    user router and controller added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import UserRouter from './routes/user.routes.js ';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -17,3 +18,5 @@ const app = express() ;
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000'); 
 })
+
+app.use('/api/user',UserRouter); 
---
+++ routes/user.routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { test } from "../controllers/user.controller.js";
+
+const UserRouter = express.Router();
+
+UserRouter.get('/test',test);
+
+export default UserRouter ; 
+
