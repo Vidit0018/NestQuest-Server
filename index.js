54c411b760d148201ca2b3aa57db2fcb425f711a
Vidit Prajapati <vidit.msit@gmail.com>
2024-05-26 18:47:45 +0530

    signup api route completed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import UserRouter from './routes/user.routes.js ';
 import dotenv from 'dotenv';
+import AuthRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -14,9 +15,10 @@ mongoose
 })
 
 const app = express() ;
-
+app.use(express.json())
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000'); 
 })
 
 app.use('/api/user',UserRouter); 
+app.use('/api/auth',AuthRouter); 
