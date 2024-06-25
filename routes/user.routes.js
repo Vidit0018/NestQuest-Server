ee4be8c4ab8cfbee9f838ec9ca2e5beac93eb091
Vidit Prajapati <vidit.msit@gmail.com>
2024-06-25 22:25:24 +0530

    contacting landlord feature added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { test,updateUser,deleteUser , getUserListing } from "../controllers/user.controller.js";
+import { test,updateUser,deleteUser , getUserListing ,getUser} from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const UserRouter = express.Router();
 
@@ -7,6 +7,7 @@ UserRouter.get('/test',test);
 UserRouter.post('/update/:id',verifyToken,updateUser)
 UserRouter.delete('/delete/:id',verifyToken,deleteUser)
 UserRouter.get('/listings/:id',verifyToken,getUserListing)
+UserRouter.get('/:id',verifyToken,getUser)
 
 export default UserRouter ; 
 
