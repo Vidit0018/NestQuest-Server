42b15715817fea03d56844e2c8325e7a16a7141e
Vidit Prajapati <vidit.msit@gmail.com>
2024-06-27 01:08:48 +0530

    cors changes made

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import AuthRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
 dotenv.config();
 
 mongoose
@@ -19,7 +20,7 @@ mongoose
 const app = express() ;
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(cors()); 
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000'); 
