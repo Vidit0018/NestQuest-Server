88d12b1b27f6c12b731dc7f90a334106f9d60f84
Vidit Prajapati <vidit.msit@gmail.com>
2024-06-27 14:33:24 +0530

    cors changes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,6 +2,7 @@ import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
 import Listing from '../models/listing.model.js';
+
 export const test = (req,res)=>{
     res.json({
         message: 'Test Route',
--- index.js
+++ index.js
@@ -20,7 +20,12 @@ mongoose
 const app = express() ;
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors()); 
+app.use(cors({
+    origin: '*',
+    credentials: true
+  }));
+  
+  
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000'); 
