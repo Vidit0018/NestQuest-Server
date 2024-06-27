f46061779caf8d37f5f46cd1ac75206999fe8b1f
Vidit Prajapati <vidit.msit@gmail.com>
2024-06-27 14:44:41 +0530

    cookie settings changed

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -31,7 +31,11 @@ export const signin = async(req,res,next)=>{
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
         const {password:pass,...rest}=validUser._doc;
         res
-        .cookie('access_token', token, { httpOnly: true })
+        .cookie('access_token', token, {
+             httpOnly: true ,
+             secure: true, 
+             sameSite: 'None' 
+         })
         .status(200)
         .json(rest);
      } catch (error) {
@@ -45,7 +49,9 @@ export const google = async ( req,res,next)=>{
             const token = jwt.sign({id:user._id},process.env.JWT_SECRET);
             const {password: pass, ...rest} = user._doc;
             res
-            .cookie('access_token', token , { httpOnly : true})
+            .cookie('access_token', token , { httpOnly: true ,
+                secure: true, 
+                sameSite: 'None' })
             .status(200)
             .json(rest);
         }
@@ -61,7 +67,9 @@ export const google = async ( req,res,next)=>{
             await newUser.save();
             const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET);
             const {password: pass, ...rest} = newUser._doc;
-            res.cookie('access_token', token, {httpOnly:true}).status(200).json(rest);
+            res.cookie('access_token', token, {httpOnly: true ,
+                secure: true, 
+                sameSite: 'None' }).status(200).json(rest);
         }
     } catch (error) {
         next(error);
